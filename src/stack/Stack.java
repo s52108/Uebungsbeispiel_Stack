be88945872cc80cb2b16c2174b9db80637849226
postl <s52108@edu.campus02.at>
2022-11-04 20:25:45 +0100

    Added Order.Java & OrderLine

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,6 +1,45 @@
 package stack;
 
-public class Stack {
-    private int elements[];
-    private int counter=-1;
+public class Stack<T>{
+    private T value;
+    private String colour;
+
+    /*    public stack.Card(int value, String colour) {
+            if (value < 1 || value > 9) {
+                System.out.println("Nicht erlaubter Wert: " + value);
+                return;
+            }
+            if (colour.equals("blue") || colour.equals("green") || colour.equals("red") || colour.equals("yellow")) {
+                this.value = value;
+                this.colour = colour;
+            } else System.out.println("Nicht erlaubte Farbe: " + colour);
+        }*/
+
+    public Card(T value, String colour) {
+        if (value < 1 || value > 9) {
+            System.out.println("Nicht erlaubter Wert: " + value);
+            return;
+        }
+        if (!(colour.equals("blue") || colour.equals("green") || colour.equals("red") || colour.equals("yellow"))) {
+            System.out.println("Nicht erlaubte Farbe: " + colour);
+            return;
+        }
+        this.value = value;
+        this.colour = colour;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String toString() {
+        return "stack.Card{" +
+                "value=" + value +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
 }
